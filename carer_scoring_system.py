f62823469f67566588fabef2be358496f898d60a
unknown <a.macheny@outlook.com>
2021-11-17 14:49:26 +0000

    some practice notes

--- carer_scoring_system.py
+++ carer_scoring_system.py
@@ -1,1 +1,14 @@
 print("Hello World")
+testList = ["princess", "beverley"]
+print("Test list is " + testList[0])
+
+myTuple = ("asparagus", "bottle", "carrot")
+print("Tuple is " + str(myTuple))
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print("This dictionary is " + str(thisdict))
+print(thisdict["year"])
