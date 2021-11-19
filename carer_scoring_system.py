497b6cd3f53fda8d27f98dc3726f80a573a68bd4
unknown <a.macheny@outlook.com>
2021-11-19 18:52:48 +0000

    Added prev clients, last logon and image problems

--- carer_scoring_system.py
+++ carer_scoring_system.py
@@ -24,6 +24,8 @@
 # (e.g. ) num previous clients determines whether the days since last logon is a red flag. But we only need to know whether they have 0 previous clients. 
 # Becuase if the nad no previous clients, and havent logged on in moths, they are a much worse fit that someone who has at least had a client, no matter how long ago they logged on
 
+# REMOVE EXTRA UNNORMALISED VARIABLES IF NOT NEEDED
+
 
 ### Start of program ###
 import pandas as pd
@@ -43,8 +45,51 @@ carer_data = pd.read_csv('data.csv')
 min_max_scaler = preprocessing.MinMaxScaler() #create a scaler object
 years_exp = np.array(carer_data['years_experience']).reshape((len(carer_data['years_experience']), 1)) # extract years experience column and make it a 2d list/array
 n_years_exp = min_max_scaler.fit_transform(years_exp)*100 # fit and then normalise values, and then multiply by 100 to get score
+print("\n\nNormalised Years experience: \n")
 print(n_years_exp)
 
 # AVERAGE REVIEW
 # Extract and modify according to the number of reviews, i.e. if the number of reviews are 0, take one point off. Then normalise data
-avg_num_reviews = carer_data[['avg_review','num_reviews']] # extract years experience column and make it a 2d list/array
+avg_num_reviews = np.array(carer_data[['num_reviews','avg_review']]) # extract years experience column and make it a 2d list/array
+avg_num_reviews[avg_num_reviews[:, 0] == 0, 1] = avg_num_reviews[avg_num_reviews[:, 0] == 0, 1] - 1 # if the number of reviews is 0, reduce the average review score by 1 point
+print("\n\nAverage and number of review ")
+print(avg_num_reviews)
+n__average_rev = min_max_scaler.fit_transform(np.array(avg_num_reviews[:, 1]).reshape((len(avg_num_reviews[:, 1]), 1)))*100 # fit and then normalise values, and then multiply by 100 to get score
+print("\n\nNormalised Average review ")
+print(n__average_rev)
+
+# NUMBER OF PREVIOUS CLIENTS
+# Extract and normalise data
+# # This has less of an impact becuase say they have 0 previous clients but have 20 years experience, they are still very suited! but experience through the app is still important 
+# because the brand may require different things, and it is more reliable
+num_prev_clients = np.array(carer_data['num_previous_clients']).reshape((len(carer_data['num_previous_clients']), 1)) # extract no previous clients column and make it a 2d list/array
+n_num_previous_cli = min_max_scaler.fit_transform(num_prev_clients)*50 # fit and then normalise values, and then multiply by 50 to get score
+print("\n\nNumber of Previous Clients: \n")
+print(n_num_previous_cli)
+
+# DAYS SINCE LAST LOGON
+# Extract data, and modify according to the number of previous clients to create a greater disparity between those who have logged in a while ago due to having current clients, 
+# and those who have had no clients and signed up for the banter
+days_since_log = np.array(carer_data['days_since_login']) # extract days since logon
+mean_login = np.mean(days_since_log) # get average logon time
+three_quart_percentile = np.percentile(days_since_log, 75) # get lower percentile logon time
+print("\n\nMean login time is ")
+print(mean_login)
+print("\n\n75 percent login time is ")
+print(three_quart_percentile)
+days_prev_clients = np.array(carer_data[['num_previous_clients', 'days_since_login']])
+days_prev_clients[np.logical_and(days_prev_clients[:, 0] == 0, days_prev_clients[:, 1] > mean_login, days_prev_clients[:, 1] <= three_quart_percentile), 1] = days_prev_clients[np.logical_and(days_prev_clients[:, 0] == 0, days_prev_clients[:, 1] > mean_login, days_prev_clients[:, 1] <= three_quart_percentile), 1] * 2 # if no of previous clients is 0, make the days since last login half if it is greater than the average
+days_prev_clients[np.logical_and(days_prev_clients[:, 0] == 0, days_prev_clients[:, 1] > three_quart_percentile), 1] = days_prev_clients[np.logical_and(days_prev_clients[:, 0] == 0, days_prev_clients[:, 1] > three_quart_percentile), 1] *4 # if no of previous clients is 0, make the days since last login a quarter if it is greater than the quarter
+print("\n\n Number previous clients and Days since last login : ")
+print(days_prev_clients)
+n_days_since_log = (1-min_max_scaler.fit_transform(days_prev_clients[:, 1].reshape(len(days_prev_clients[:, 1]),1))) * 25
+print("\n\n Normalised Days since last logon")
+print(n_days_since_log)
+
+# IMAGE PROBLEMS
+img_problems = num_prev_clients = np.array(carer_data['img_problems']).reshape((len(carer_data['img_problems']), 1))
+n_image_probs = (1-min_max_scaler.fit_transform(img_problems)) * 25
+print("\n\nNormalised Image Problems")
+print(n_image_probs)
+
+#Total_scores = [n_years_exp + n__average_rev + n_num_previous_cli + n_days_since_log + n_image_probs]
